4dbe705108f2e60160f099619cecc4ea3266d756
Your Name <zhongbenhua@hotmail.com>
2017-08-02 10:58:53 +0800

    first commit

---
+++ framework/contract/core.js
@@ -0,0 +1,67 @@
+module.exports = {
+
+  deposit: async function (currency, amount, srcId, recipientId) {
+    if (!recipientId) return 'Invalid recipient'
+
+    app.validate('amount', amount)
+
+    if (app.meta.delegates.indexOf(this.trs.senderPublicKey) === -1) {
+      return 'Sender is not a delegate'
+    }
+
+    // TODO validate src transaction and params
+
+    let exists = await app.model.Deposit.exists({ srcId: srcId })
+    if (exists) return 'Double deposit'
+
+    app.sdb.lock('core.deposit@' + srcId)
+
+    app.balances.increase(recipientId, currency, amount)
+    app.sdb.create('Deposit', {
+      tid: this.trs.id,
+      currency: currency,
+      amount: amount,
+      srcId: srcId,
+      recipientId: recipientId
+    })
+  },
+
+  withdrawal: async function (currency, amount) {
+    app.validate('amount', amount)
+
+    var balance = app.balances.get(this.trs.senderId, currency)
+
+    if (balance.lt(amount)) return 'Insufficient balance'
+    app.balances.decrease(this.trs.senderId, currency, amount)
+  },
+
+  transfer: async function (currency, amount, recipientId) {
+    if (!recipientId) return 'Invalid recipient'
+
+    var balance = app.balances.get(this.trs.senderId, currency)
+
+    if (this.block.height !== 1 && balance.lt(amount)) return 'Insufficient balance'
+    app.balances.transfer(currency, amount, this.trs.senderId, recipientId)
+    app.sdb.create('Transfer', {
+      tid: this.trs.id,
+      senderId: this.trs.senderId,
+      recipientId: recipientId,
+      currency: currency,
+      amount: amount
+    })
+  },
+
+  setNickname: async function (name) {
+    if (!name || name.length > 20) return 'Invalid name'
+
+    app.sdb.lock('core.account@' + this.trs.senderId)
+
+    let exists = await app.model.Account.exists({ str1: name })
+    if (exists) return 'Nickname already exists'
+
+    app.sdb.replace('Account', {
+      address: this.trs.senderId,
+      str1: name
+    })
+  }
+}
